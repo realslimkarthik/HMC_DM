b678a5cbc65bcad99c384951f2a3dcc4b6c04dd2
Karthik Hariharan <karthikhariharan13@gmail.com>
2015-01-14 13:16:52 -0600

    All files in a single directory pieced together
    Tried to go through all files in a directory to put incomplete pieces
    back together

--- dm_fileComplete.py
+++ dm_fileComplete.py
@@ -1,10 +1,47 @@
 import sys
+import os
+
+path = "C:\Users\kharih2\Work\DM_Karthik\HMC_Data\\"
+platform = {'tw':{'dirName': "TwitterPowerTrack", 'format': "json"}, 
+            'fb': {'dirName': "Facebook", 'format': "json"}
+            }
+
+def fixFilesbyDir(path, openBracket, closeBracket):
+    files = os.listdir(path)
+    for i in range(len(files)):
+        try:
+            f1 = open(files[i], 'w')
+            f2 = open(files[i + 1], 'r')
+            contentsf1 = f1.readlines()
+            for j in contentsf1[-1]:
+                if i == openBracket:
+                    counter += 1
+                elif i == closeBracket:
+                    counter -= 1
+            if counter != 0:
+                contentsf2 = f2.readlines()
+                for j in contentsf2[0]:
+                    if i == openBracket:
+                        counter += 1
+                    elif i == closeBracket:
+                        counter -= 1
+                if counter != 0:
+                    f1.write(contentsf2[0])
+            f1.close()
+            f2.close()
+        except IndexError:
+            # Repeat for the next dir
+            print "Hello"
+            f1.close()
+            return path + files[i]
+
+def fixFilesWrapper(path):
+    
 
 if __name__ == "__main__":
-    firstFile = sys.argv[1]
-    #secondFile = sys.argv[2]
-    f = open(firstFile)
-    contents = f.readline()
-    print len(contents)
-    lastLine = contents[len(contents) - 1]
-    print lastLine
+    sys.argv[1]
+    path += platform[sys.argv[1]]['dirName'] + "\September-2014-Master\\"
+    #print os.listdir(path)
+    lastFile = fixFilesbyDir(path, '{', '}')
+    print lastFile
+    
