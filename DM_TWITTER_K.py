f14973a854ab8d3a5e7441bcba7abf8700fe8829
Karthik Hariharan <karthikhariharan13@gmail.com>
2015-01-30 14:52:05 -0600

    Fixed twitter_entities_mentions bug

--- DM_TWITTER_K.py
+++ DM_TWITTER_K.py
@@ -17,12 +17,12 @@ def CSVfromTwitterJSON(jsonfilename, collName, DorP, errorfile=None, overwrite=F
     if (not os.path.isfile(jsonfilename+".csv")) or overwrite:
         jsonfile = open(jsonfilename, 'r')
         #Will track all variables seen across all tweets in the file
-        mykeys = []
         #Will contain a dictionary for each processed tweet
         tweetList = []
         conf = ConfigParser.ConfigParser()
         conf.read("fields.cfg")
-        
+        mykeys = []
+
         for line in jsonfile:
             myline = string.strip(line)
             if myline != "":
@@ -59,14 +59,14 @@ def CSVfromTwitterJSON(jsonfilename, collName, DorP, errorfile=None, overwrite=F
                         print tweet['info']['activity_count']
                     else:
                         #Create an empty dictionary
-                        a = {}
+                        tweetObj = {}
                         #Send the JSON dictionary, the empty dictionary, and the list of all keys
-                        extract(tweet, a, mykeys)
+                        extract(tweet, tweetObj, mykeys)
                         #Add the output dictionary to the list
-                        tweetList.append(a)
+                        populateMongo(tweetObj, collName, DorP)
+                        # tweetList.append(tweetObj)
         #Print the number of tweets processed
         jsonfile.close()
-        populateMongo(tweetList, mykeys, collName, DorP)
         # printCSV(csvfile, tweetList, mykeys)
         print "Finished populating collection ", collName
 
@@ -83,7 +83,17 @@ def removeKey(key):
 #Recursive function to process the input dictionary
 def extract(DictIn, Dictout, allkeys, nestedKey=""):
     #If DictIn is a dictionary
-    if isinstance(DictIn, dict):
+    if nestedKey == "twitter_entities_user_mentions":
+        Dictout["entitiesusrmentions"] = []
+        mentionSet = set()
+        inObj = {}
+        for i in DictIn:
+            inObj['is'] = i['id_str']
+            inObj['n'] = i['name']
+            inObj['sn'] = i['screen_name']
+            Dictout["entitiesusrmentions"].append(inObj)
+            inObj = {}
+    elif isinstance(DictIn, dict):
         #Process each entry
         for key, value in DictIn.iteritems():
             #If nested, prepend the previous variables
@@ -142,7 +152,7 @@ def extract(DictIn, Dictout, allkeys, nestedKey=""):
                     Dictout[newKey] = ""
 
 # ========================================================================================
-def populateMongo(inputJson, mykeys, collName, DorP):
+def populateMongo(inputTweet, collName, DorP):
     host = conf.get(DorP, "host")
     port = int(conf.get(DorP, "port"))
     fieldConf = ConfigParser.ConfigParser()
@@ -150,50 +160,47 @@ def populateMongo(inputJson, mykeys, collName, DorP):
     client = MongoClient(host, port)
     db = client.twitter
     collection = db[collName]
-    # ruleConf = ConfigParser.ConfigParser()
-    # ruleConf.read("rules.cfg")
     r = open("rules.json")
     ruleConf = json.loads(r.read())
     mongoConf = ConfigParser.ConfigParser()
     mongoConf.read("fieldsToMongo.cfg")
+    if 'entitieshtagstext' not in inputTweet:
+        inputTweet['entitieshtagstext'] = []
+    else:
+        #packing the entitieshtagstext field into an array
+        inputTweet['entitieshtagstext'] = inputTweet['entitieshtagstext'].split(';')
 
-    for i in inputJson:
-        if 'entitieshtagstext' not in i:
-            i['entitieshtagstext'] = []
-        else:
-            #packing the entitieshtagstext field into an array
-            i['entitieshtagstext'] = i['entitieshtagstext'].split(';')
+    # Renaming id field
+    inputTweet['_id'] = "tw" + inputTweet['Idpost'].split(':')[2]
+    logging.info('Started posting collection with id: ' + inputTweet['_id'] + ' into collection ' + collName)
+    inputTweet.pop('Idpost', None)
+    # packing the matchingrulesvalue field into an array
+    inputTweet['matchingrulesvalue'] = inputTweet['matchingrulesvalue'].split(';')
 
-        # Renaming id field
-        i['_id'] = "tw" + i['Idpost'].split(':')[2]
-        logging.info('Started posting collection with id: ' + i['_id'] + ' into collection ' + collName)
-        i.pop('Idpost', None)
-        # packing the matchingrulesvalue field into an array
-        i['matchingrulesvalue'] = i['matchingrulesvalue'].split(';')
+    # Changing postedTime into ISO format for processing using JavaScript in Mongo
+    dateFrag = inputTweet['postedTime'].split('T')
+    dateFrag[1] = dateFrag[1].split('.')[0]
+    dateStr = ''.join(' ' + d for d in dateFrag).strip()
+    timeStruct = time.strptime(dateStr, "%Y-%m-%d %H:%M:%S")
+    dateObj = datetime.fromtimestamp(time.mktime(timeStruct))
+    inputTweet['postedTime'] = dateObj
+    
+    inputTweet['ruleIndex'] = []
+    for j in inputTweet['matchingrulesvalue']:
+        inputTweet['ruleIndex'].append(ruleConf[j.strip()])
+    inputTweet.pop('matchingrulesvalue', None)
+    print inputTweet['ruleIndex']
+    mongoRecord = {}
+    
+    for (key, val) in inputTweet.iteritems():
+        newKey = mongoConf.get('fields', key)
+        mongoRecord[newKey] = val    
 
-        # Changing postedTime into ISO format for processing using JavaScript in Mongo
-        dateFrag = i['postedTime'].split('T')
-        dateFrag[1] = dateFrag[1].split('.')[0]
-        dateStr = ''.join(' ' + d for d in dateFrag).strip()
-        timeStruct = time.strptime(dateStr, "%Y-%m-%d %H:%M:%S")
-        dateObj = datetime.fromtimestamp(time.mktime(timeStruct))
-        i['postedTime'] = dateObj
-        
-        i['ruleIndex'] = []
-        for j in i['matchingrulesvalue']:
-            i['ruleIndex'].append(ruleConf[j.strip()])
-        i.pop('matchingrulesvalue', None)   
-        print i['ruleIndex']
-        mongoRecord = {}
-        for (key, val) in i.iteritems():
-            newKey = mongoConf.get('fields', key)
-            mongoRecord[newKey] = val
-
-        try:
-            collection.insert(mongoRecord)
-        except KeyError:
-            logging.debug("Duplicate tweet _id=" + i['_id'])
-    # outputFile.write(json.dumps(inputJson, ensure_ascii=False).encode('utf-8'))
+    try:
+        collection.insert(mongoRecord)
+    except KeyError:
+        logging.debug("Duplicate tweet _id=" + inputTweet['_id'])
+    # outputFile.write(json.dumps(inputTweet, ensure_ascii=False).encode('utf-8'))
 
 # ========================================================================================
 
@@ -222,23 +229,6 @@ if __name__ == "__main__":
             outF.close()
         elif op == "count":
             flattenJSON(inputFile)
-    elif choice == "interactive":
-        print "Month is August for now"
-        # mon = input("Enter the month you want:")[0:3].lower()
-        day = input("Enter the timeframe that you want: (1, 7, 15, 30)")
-        mon = "Aug"[0:3].lower()
-        src_path = conf.get("twitter", "prod_src_path").format(current_month)
-        dest_path = conf.get("twitter", "prod_dest_path").format(current_month)
-
-        exDays = ['jan', 'mar', 'may', 'jul', 'aug', 'oct', 'dec']
-        lastday = daysInMonth(mon)
-        dayDict = {'1': {'ranges': range(lastday + 1), 'fileNames': map(lambda x: '_' + str(x), range(lastday + 1))},
-                    '7': {'ranges': [8, 16, 24], 'fileNames': ['_1_7', '_8_15', '_16_23', '_24_' + str(lastday)]},
-                    '15': {'ranges': [16], 'fileNames': ['_1_15', '_16_' + str(lastday)]},
-                    '30': {'ranges': [], 'fileNames': ['']}
-                    }
-        csvFiles = [dest_path + dayDict[day]['fileNames'][0]]
-
     elif choice =="prod":
         logging.basicConfig(filename='prodUpload.log', level=logging.DEBUG)
         op = sys.argv[2]
@@ -255,25 +245,15 @@ if __name__ == "__main__":
                     logging.info("Started uploading " + j)
                     if int(fileName) < 6:
                         CSVfromTwitterJSON(src_path + j, collName + "_1", "mongo")
-                    elif int(fileName) < 11:
-                        CSVfromTwitterJSON(src_path + j, collName + "_2", "mongo")
-                    elif int(fileName) < 16:
-                        CSVfromTwitterJSON(src_path + j, collName + "_3", "mongo")
-                    elif int(fileName) < 21:
-                        CSVfromTwitterJSON(src_path + j, collName + "_4", "mongo")
-                    elif int(fileName) < 26:
-                        CSVfromTwitterJSON(src_path + j, collName + "_5", "mongo")
-                    elif int(fileName) < 32:
-                        CSVfromTwitterJSON(src_path + j, collName + "_6", "mongo")
-
+                    # elif int(fileName) < 11:
+                    #     CSVfromTwitterJSON(src_path + j, collName + "_2", "mongo")
+                    # elif int(fileName) < 16:
+                    #     CSVfromTwitterJSON(src_path + j, collName + "_3", "mongo")
+                    # elif int(fileName) < 21:
+                    #     CSVfromTwitterJSON(src_path + j, collName + "_4", "mongo")
+                    # elif int(fileName) < 26:
+                    #     CSVfromTwitterJSON(src_path + j, collName + "_5", "mongo")
+                    # elif int(fileName) < 32:
+                    #     CSVfromTwitterJSON(src_path + j, collName + "_6", "mongo")
 
             # TO DO: Implement Time Frame based uploading
-
-            # timeFrame = range(6)[1:]
-            # for i in timeFrame:
-
-            #     for j in fileList:
-            #         if len(j.split('-') == 3):
-            #             if int(j.split('-')[-1].split('.')[0]) == i:
-            #                 CSVfromTwitterJSON(src_path + j, csvfile, 'mongo')
-            #                 print j
