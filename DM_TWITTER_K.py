394111fcf4839b71e95b12d39be42a57300b01db
Karthik Hariharan <karthikhariharan13@gmail.com>
2015-02-13 16:21:16 -0600

    Removed dynamic rule addition

--- DM_TWITTER_K.py
+++ DM_TWITTER_K.py
@@ -76,7 +76,7 @@ def CSVfromTwitterJSON(jsonfilename, collName, DorP, errorfile=None, overwrite=F
                         #Send the JSON dictionary, the empty dictionary, and the list of all keys
                         extract(tweet, tweetObj, mykeys)
                         #Add the output dictionary to the list
-                        populateMongo(tweetObj, collName, DorP, configData)
+                        populateMongo(tweetObj, collName, DorP, ruleConf, configData)
                         try:
                             print tweetObj['geocoordinates']
                         except KeyError:
@@ -191,21 +191,21 @@ def extract(DictIn, Dictout, allkeys, nestedKey=""):
                     Dictout[newKey] = ""
 
 # ========================================================================================
-def addRule(rule):
-    r = open(conf_path.format("rules.json", "a"))
-    num_lines += 1
-    count = 1
-    for i in r:
-        if count == num_lines:
-            i.replace('}', "\"" + rule + "\"" + : "\"" + str(num_lines) + "\"" + '\n')
-        count += 1
-    r.write('}')
-    r.close()
-    num_lines += 1
-    ruleConf = json.loads(r.read())
+# def addRule(rule):
+#     r = open(conf_path.format("rules.json", "a"))
+#     num_lines += 1
+#     count = 1
+#     for i in r:
+#         if count == num_lines:
+#             i.replace('}', "\"" + rule + "\"" + ':' + "\"" + str(num_lines) + "\"" + '\n')
+#         count += 1
+#     r.write('}')
+#     r.close()
+#     num_lines += 1
+#     ruleConf = json.loads(r.read())
 
 # ========================================================================================
-def populateMongo(inputTweet, collName, DorP, configData):
+def populateMongo(inputTweet, collName, DorP, ruleConf, configData):
     host = configData[0]
     port = configData[1]
     # Creating a new MongoDB client
@@ -243,10 +243,12 @@ def populateMongo(inputTweet, collName, DorP, configData):
             inputTweet['ruleIndex'].append(int(ruleConf[j.strip()]))
         except KeyError:
             # If the rule isn't found in the ruleConf, then add the rule to the rules.json file
-            addRule(j.strip())
-            logging.debug("Invalid rule fetched via GNIP with _id=" + inputTweet['_id'] + " with rule=" + j.strip())
+            # addRule(j.strip())
+            logging.warning("Invalid rule fetched via GNIP with _id=" + inputTweet['_id'] + " with rule=" + j.strip())
+            logging.debug(str(inputTweet))
             print "Invalid rule fetched via GNIP with _id=" + inputTweet['_id'] + " with rule=" + j.strip()
-            inputTweet['ruleIndex'].append(int(ruleConf[j.strip()]))
+
+            # inputTweet['ruleIndex'].append(int(ruleConf[j.strip()]))
             # Add the new rule that was added in the ruleConf
     # Remove the former matchingrulesvalue key
     inputTweet.pop('matchingrulesvalue', None)
@@ -271,8 +273,10 @@ def populateMongo(inputTweet, collName, DorP, configData):
 
 # ========================================================================================
 # Command to run the script
-# python DM_TWITTER_K.py prod <Capitalized full month name> <Four digit year>
-# Eg - python DM_TWITTER_K.py prod August 2014
+# python DM_TWITTER_K.py prod <Capitalized 3 letter month name> <Four digit year>
+# Eg - python DM_TWITTER_K.py prod Aug 2014
+
+monthToNames = {'Jan': '01', 'Feb': '02', 'Mar': '03', 'Apr': '04', 'May': '05', 'Jun': '06', 'Jul': '07', 'Aug': '08', 'Sep': '09', 'Oct': '10', 'Nov': '11', 'Dec': '12'}
 
 if __name__ == "__main__":
     choice = sys.argv[1]
@@ -291,19 +295,20 @@ if __name__ == "__main__":
         current_year = sys.argv[3]
         # Format the input month and year to form a a part of the Mongo Collection name
         collName = current_month[0:3] + current_year[2:]
+        print collName
         # Get the path for the logs output
         logs = conf.get("conf", "prod_log_path")
         logging.basicConfig(filename=logs.format('(prodUpload' + collName +'.log'), level=logging.DEBUG)
         # Get the path for the source Raw_data json files
-        src_path = conf.get("twitter", "prod_src_path").format(current_month + '-' + current_year)
+        src_path = conf.get("twitter", "prod_src_path").format(current_year + current_month)
         # Get the list of files in the source directory
         fileList = os.listdir(src_path)
         # Iterate over every file in the source directory
         for j in fileList:
-            # If it's a by-day file in the source directory it will have 3 parts around a '-'
-            if len(j.split('-')) == 3:
+            # If it's a by-day file in the source directory it will have 3 parts around the '-'s
+            if len(j.split('_')) == 3:
                 # Extract the date of the corresponding file from it's name
-                fileDate = int(j.split('-')[-1].split('.')[0])
+                fileDate = int(j.split('_')[-1].split('.')[0])
                 logging.info("Started uploading " + j)
                 # Upload to the corresponding Mongo Collection based on the date extracted from the file
                 if fileDate < 6:
