0aae1d5cd0ebbc270ab341c353ad2ec9485a5e92
Karthik Hariharan <karthikhariharan13@gmail.com>
2015-02-13 16:22:19 -0600

    Updated Month number

--- DM_Youtube.py
+++ DM_Youtube.py
@@ -5,7 +5,7 @@ import ConfigParser
 def aggregateByDay(year, conf):
     src_parts_path = conf.get("youtube", "prod_src_parts_path")
     src_path = conf.get("youtube", "prod_src_path")
-    for i in range(12, 13):
+    for i in range(7, 13):
         for j in range(1, 32):
             parts_src = src_parts_path.format(year, str(i).zfill(2), str(j).zfill(2))
             src = src_path.format(year, str(i).zfill(2))
