c9f611a7e5f59db4b47def1648cd8d352c395f86
Karthik Hariharan <karthikhariharan13@gmail.com>
2015-03-18 15:10:01 -0500

    Finished rule counting

--- countRulesYT.py
+++ countRulesYT.py
@@ -10,39 +10,53 @@ def countRules(filename, rules):
     f = open(filename)
     ruleCounts = {}
     tagCounts = {}
-    soup = BeautifulSoup(f.read())
+    line = ""
+    for i in f.readlines():
+        if '</entry>' in i:
+            line += i
+            counts = extractRule(line, rules)
+            line = ""
+            if counts[0] != None:
+                if counts[0] in tagCounts:
+                    tagCounts[counts[0]] += 1
+                else:
+                    tagCounts[counts[0]] = 1
+                if counts[0] in ruleCounts:
+                    ruleCounts[counts[0]] += 1
+                else:
+                    ruleCounts[counts[0]] = 1
+        else:
+            line += i
+
     f.close()
-    for i in soup.find_all("gnip:matching_rule"):
-        rule = i.get_text()
-        if rule != "":
-            try:
-                tag = rules[rule]
-            except KeyError:
-                print rule
-                print filename
-                sys.exit(1)
-                tag = rules[rule.strip("\"")]
-            if tag in tagCounts:
-                tagCounts[tag] += 1
-            else:
-                tagCounts[tag] = 1
-            if rule in ruleCounts:
-                ruleCounts[rule] += 1
-            else:
-                ruleCounts[rule] = 1
-    del soup
     return (tagCounts, ruleCounts)
 
 
+def extractRule(line, rules):
+    soup = BeautifulSoup(line)
+    try:
+        rule = soup.find('gnip:matching_rule').get_text()
+        tag = rules[rule]
+    except (AttributeError, KeyError):
+        try:
+            rule = soup.find('gnip:rule').get_text()
+            tag = rules[rule]
+            print rule
+        except (AttributeError, KeyError):
+            tag = None
+            rule = None
+    return (tag, rule)
+
 def writeCounts(counts, csvfile):
-    tagWriter = CSVUnicodeWriter(csvfile)
-    tagWriter.writerow(["Tag", "Count"])
+    writer = CSVUnicodeWriter(csvfile)
+    writer.writerow(["Tag", "Count"])
     for (key, val) in counts.iteritems():
-        tagWriter.writerow([key, str(val)])
+        writer.writerow([key, str(val)])
     
 
 if __name__ == "__main__":
-    year = sys.argv[1]
+    op = sys.argv[1]
+    year = sys.argv[2]
     conf = ConfigParser.ConfigParser()
     conf.read("config\config.cfg")
     conf_path = conf.get("conf", "conf_path")
@@ -51,22 +65,45 @@ if __name__ == "__main__":
     r = open(conf_path.format('yt_rulesToTags.json'))
     rules = json.loads(r.read())
     r.close()
-    for i in range(10, 13):
-        src = src_path.format(year, str(i).zfill(2))
-        dest = dest_path.format(year, str(i).zfill(2)) + 'COUNTS\\'
-        try:
-            fileList = os.listdir(src)
-        except IOError, WindowsError:
-            print e
-            continue
-        for j in fileList:
-            if "xml" in j and "v3" in j and "error" not in j:
-                print j
-                overallCounts = countRules(src + j, rules)
-                filename = j.split('.')[0]
-                tagCsvfile = open(dest + filename + "_tags" + ".csv", "w")
-                writeCounts(overallCounts[0], tagCsvfile)
-                tagCsvfile.close()
-                ruleCsvfile = open(dest + filename + "_rules" + ".csv", "w")
-                writeCounts(overallCounts[1], ruleCsvfile)
-                ruleCsvfile.close()
+    if op == "yearly":
+        for i in range(1, 13):
+            src = src_path.format(year, str(i).zfill(2))
+            dest = dest_path.format(year, str(i).zfill(2)) + 'COUNTS\\'
+            try:
+                fileList = os.listdir(src)
+            except IOError, WindowsError:
+                print e
+                continue
+            for j in fileList:
+                if "xml" in j and "v3" in j and "error" not in j:
+                    print j
+                    overallCounts = countRules(src + j, rules)
+                    filename = j.split('.')[0]
+                    tagCsvfile = open(dest + filename + "_tags" + ".csv", "w")
+                    writeCounts(overallCounts[0], tagCsvfile)
+                    tagCsvfile.close()
+                    ruleCsvfile = open(dest + filename + "_rules" + ".csv", "w")
+                    writeCounts(overallCounts[1], ruleCsvfile)
+                    ruleCsvfile.close()
+    elif op == "interval":
+        start_month = sys.argv[3]
+        end_month = sys.argv[4]
+        for i in range(start_month, end_month):
+            src = src_path.format(year, str(i).zfill(2))
+            dest = dest_path.format(year, str(i).zfill(2)) + 'COUNTS\\'
+            try:
+                fileList = os.listdir(src)
+            except IOError, WindowsError:
+                print e
+                continue
+            for j in fileList:
+                if "xml" in j and "v3" in j and "error" not in j:
+                    print j
+                    overallCounts = countRules(src + j, rules)
+                    filename = j.split('.')[0]
+                    tagCsvfile = open(dest + filename + "_tags" + ".csv", "w")
+                    writeCounts(overallCounts[0], tagCsvfile)
+                    tagCsvfile.close()
+                    ruleCsvfile = open(dest + filename + "_rules" + ".csv", "w")
+                    writeCounts(overallCounts[1], ruleCsvfile)
+                    ruleCsvfile.close()
