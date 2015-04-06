f8ba521728299cb19593b0674c83a6ed2f63436c
Karthik Hariharan <karthikhariharan13@gmail.com>
2015-04-06 17:13:03 -0500

    Added new functions
    Can hopefully load this as an external file into the mongo shell for
    easier querying

--- mongoRef.js
+++ mongoRef.js
@@ -1,19 +1,19 @@
-Commonly used MongoDB queries:
+// Commonly used MongoDB queries:
 
 // Query for getting data after a date:
-db.Aug14_1.find({'pT': {$gte: new Date('Sep 1, 2014')}})
+db.coll_name.find({'pT': {$gte: new Date('Sep 1, 2014')}})
 
 // Query for checking if a value exists in an array:
-db.Oct14_1.find({'mrv': {'$in': ['22']}})
+db.coll_name.find({'mrv': {'$in': ['22']}})
 
 // Query to change mrv values into ints from strings:
-db.Sep14_6.find().forEach(function(doc) {
+db.coll_name.find().forEach(function(doc) {
     id = doc._id;
     a = [];
     doc.mrv.forEach(function(e) {
         a.push(parseInt(e));
     });
-    db.Sep14_6.update(
+    db.coll_name.update(
         {"_id": id},
         {"$set": {"mrv": a}
     });
@@ -22,14 +22,14 @@ db.Sep14_6.find().forEach(function(doc) {
 })
 
 // Query to delete records entered into the wrong collections:
-db.Sep14_6.find().forEach(function(doc) {
+db.coll_name.find().forEach(function(doc) {
     id = doc._id;
-    db.Dec14_6.remove({"_id": id});
+    db.coll_name.remove({"_id": id});
     print(id);
 })
 
 // Query to change pt values into ISODates from strings:
-db.Nov14_3.find().forEach(function(doc) {
+db.coll_name.find().forEach(function(doc) {
     var id = doc._id;
     var d;
     if(typeof doc.pt == "string") {
@@ -41,39 +41,43 @@ db.Nov14_3.find().forEach(function(doc) {
     }
 })
 
-db.Oct14_3.find().forEach(function(doc) {
-    db.Oct14_3.update({'_id':doc._id},{'$set':{'pt':new Date(doc.pt)}});
+// Query to convert date strings into ISO dates
+db.coll_name.find().forEach(function(doc) {
+    db.coll_name.update({'_id':doc._id},{'$set':{'pt':new Date(doc.pt)}});
 })
 
-db.Oct14_4.find().forEach(function(doc) {
+// Query to convert hashtags field to a list instead of a ';' delimited string
+db.coll_name.find().forEach(function(doc) {
     if (typeof doc['eumh'] == 'string') {
         print('changed!');
-        db.Oct14_4.update({'_id': doc._id}, {'$set': {'eumh': doc['eumh'].split(';')}});
+        db.coll_name.update({'_id': doc._id}, {'$set': {'eumh': doc['eumh'].split(';')}});
     }
 })
 
-db.coll_name.update({'_id': doc._id}, {'$set': {'mrtIndexed': newTags}});
-
-tags = {"antismoking": "1", "cdc Tips ii": "2", "cessation product": "3", "chew": "4",
-        "cigar/cigarillo": "5", "cigarette": "6", "ecig": "7", "general": "8",
-        "hookah": "9", "marijuana": "10", "pipe": "11", "state public health": "12",
-        "legacy campaign": "13", "goldilocks": "14"
+// Data Structures and Code to index Tags
+tags = {"antismoking": 1, "cdc tips ii": 2, "cessation product": 3, "chew": 4,
+        "cigar/cigarillo": 5, "cigarette": 6, "ecig": 7, "general": 8,
+        "hookah": 9, "marijuana": 10, "pipe": 11, "state public health": 12,
+        "legacy campaign": 13, "goldilocks": 14
 };
 
-db.coll_name.find().forEach(function(doc) {
-    t = doc['mrt'].split(';');
-    newTags = [];
-    t.forEach(function(e) {
-        element = e.toLowerCase().trim();
-        tag = tags[element];
-        if(tag == undefined)
-            {print("undefined" + element);}
-        else {
-            if (element == "legacy caompaign") {element = "legacy campaign";}
-            if (newTags.indexOf(tag) > -1) {newTags.push(tag);}
-        }
+function indexTags(coll_name) {
+    db[coll_name].ensureIndex({'mrtI': 1});
+    db[coll_name].find().forEach(function(doc) {
+        t = doc['mrt'].split(';');
+        newTags = [];
+        t.forEach(function(e) {
+            element = e.toLowerCase().trim();
+            tag = tags[element];
+            if(tag == undefined)
+                {print("undefined" + element);}
+            else {
+                if (element == "legacy caompaign") {element = "legacy campaign";}
+                if (newTags.indexOf(tag) == -1)
+                    {newTags.push(tag);}
+            }
+        });
+        db[coll_name].update({'_id': doc._id}, {'$set': {'mrtI': newTags}});
+        print(newTags);
     });
-    newTags.forEach(function(e) {
-        print(doc._id + ' ' + e);
-    })
-})
+}
