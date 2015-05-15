533dec59ebf09abec2534e1aba92284f113239bd
Karthik Hariharan <karthikhariharan13@gmail.com>
2015-05-15 16:50:47 -0500

    Used Pandas to write data to CSV

--- DM_ExtractRules.py
+++ DM_ExtractRules.py
@@ -1,18 +1,14 @@
 import sys
 import os
+import json
 import ConfigParser
 from pymongo import MongoClient
 from datetime import datetime
 import time
+import pandas as pd
 from utility import mkdir_p, CSVUnicodeWriter
 from pympler import asizeof
 
-
-#     coll_names = set()
-#     dataSet = []
-#     for i in window:
-#         for j in range(1, 7):
-#             coll_names.add(i + '_' + str(j))
 
 # ========================================================================================
 # Function to query Mongo Collection and retrieve all records matching a particular rule
@@ -36,6 +32,8 @@ def queryDB(mongoConf, month, year, filterRule, path, rtLines):
         coll_names.add(collName + '_' + str(j))
     # For each of the collections
     count = 1
+    with (conf_path.format('mongoToFields.json')) as fieldsFile:
+        mongoFields = json.loads(fieldsFile.read())
     for i in coll_names:
         coll = db[i]
         try:
@@ -46,194 +44,67 @@ def queryDB(mongoConf, month, year, filterRule, path, rtLines):
         # Maintain count to track the file number of the corresponding rule file
         # For each record returned by the query
         for k in data:
-            # Track the longest of each of the array fields
-            if 'eumh' in k:
-                lenHtag = len(k['eumh'])
-                if lenHtag > longestHtag:
-                    longestHtag = lenHtag
-            if 'eum' in k:
-                lenUM = len(k['eum'])
-                if lenUM > longestUMen:
-                    longestUMen = lenUM
-            if 'mrt' in k:
-                lenRTag = len(k['mrt'])
-                if lenRTag > longestRTag:
-                    longestRTag = lenRTag
-            if len(k['mrv']) > longestRule:
-                longestRule = len(k['mrv'])
+            modifiedObj = {}
+            # Translate field names
+            for (key, val) in k.iteritems():
+                modifiedObj[mongoFields[key]] = val
+
+            # Add extra fields for Posted Time
+            if 'postedTime' in modifiedObj:
+                modifiedObj['Year'] = date.strftime("%Y")
+                modifiedObj['Month'] = date.strftime("%b")
+                modifiedObj['Day'] = date.strftime("%d")
+                modifiedObj['Time'] = date.strftime("%H:%M:%S")
+            # If actorutcOffset is None, then initialize field with a '.'
+            if modifiedObj['actorutcOffset'] is None:
+                modifiedObj['actorutcOffset'] = '.'
+            # Unroll all the arrays
+            index = 1
+            if 'entitieshtagstext' in modifiedObj:
+                for i in modifiedObj['entitieshtagstext']:
+                    modifiedObj['entitieshtagstext' + str(index)] = i
+                    index += 1
+                del(modifiedObj['entitieshtagstext'])
+                index = 1
+            if 'entitiesusrmentions' in modifiedObj:
+                for i in modifiedObj['entitiesusrmentions']:
+                    modifiedObj['entitiesusrmentionsidstr' + str(index)] = i['is']
+                    modifiedObj['entitiesusrmentionsname' + str(index)] = i['n']
+                    modifiedObj['entitiesusrmentionssname' + str(index)] = i['sn']
+                    index += 1
+                del(modifiedObj['entitiesusrmentions'])
+                index = 1
+            # Translated and unroll matchingrulestag array
+            if 'matchingrulestag' in modifiedObj:
+                translatedTags = []
+                for i in modifiedObj['matchingrulestag']:
+                    tag = (rtLines[0][int(t)].split(':')[0]).strip('"')
+                    modifiedObj['matchingrulestag' + str(index)] = tag
+                    index += 1
+                del(modifiedObj['matchingrulestag'])
+                index = 1
+            # Unroll matchingrulesvalue array and create a new field to hold all the translated matchingrulesvalues
+            if 'matchingrulesvalue' in modifiedObj:
+                translatedRules = []
+                for i in modifiedObj['matchingrulesvalue']:
+                    modifiedObj['matchingrulesvalue' + str(index)] = i
+                    index += 1
+                    rule = ':'.join(rtLines[1][int(result[key][j])].split(':')[0:-1])
+                    translatedRules.append(rule)
+                modifiedObj['matchingrulesvalues'] = ';'.join(translatedRules)
+                del(modifiedObj['matchingrulesvalue'])
+                index = 1
             # Add the record to the dataset list
-            dataSet.append(k)
+            dataSet.append(modifiedObj)
             # If the size of the dataSet list exceeds a certain threshold, write to a file
-            # if sys.getsizeof(dataSet) > 104857600:
             if asizeof.asizeof(dataSet) > 104857600:
                 print "\nWriting to File...\n"
-                # Package all the control information into a tuple
-                outputSet = (dataSet, longestRule, longestHtag, longestUMen, longestRTag, count)
-                # Send all the control information to the PrintCSV function to convert the Dict into a CSV
-                printCSV(outputSet, path, month, filterRule, rtLines)
-                # Reset all the values and increment the count to denote that the next file must be started when writing next
-                outputSet = ()
-                dataSet = []
-                longestHtag = 0
-                longestRule = 0
-                longestUMen = 0
-                longestRTag = 0
+                # Create a new DataFrame and write to a csv file
+                df = pd.DataFrame(dataSet)
+                with open(path + month + rule + '_' + str(counter) + '.csv', 'wb') as csvfile:
+                    df.to_csv(csvfile, sep=',', index=False)
                 count += 1
-        # Close the cursor
-        data.close()
-
-    # Once iteration over all the records is done, send all control info to PrintCSV to generate CSV file
-    print "\nWriting to File...\n"
-    outputSet = (dataSet, longestRule, longestHtag, longestUMen, longestRTag, count)
-    printCSV(outputSet, path, month, filterRule, rtLines)
-
-
-# ========================================================================================
-# Function to print the Header of the CSV file
-def printHead(csvfile, resultList, delim, fields):
-    # Initialize the keyList to an empty list to hold all the keys to iterate over while printing data
-    keyList = []
-    # Iterating over all the column headings to be printed
-    for (key, val) in fields:
-        # Adding heading to keyList
-        keyList.append(key)
-        # Break postedTime into multiple fields as shown below
-        if val == "postedTime":
-            csvfile.write(val + delim + "Year" + delim + "Month" + delim + "Day" + delim + "Time" + delim)
-            continue
-        # Unroll matchingrulesvalue into individual fields with a number next to it
-        if val == "matchingrulesvalue":
-            if resultList[1] > 1:
-                csvfile.write("matchingrulesvalues" + delim)
-                for i in range(1, resultList[1] + 1):
-                    csvfile.write(val + str(i) + delim)
-                continue
-            else:
-                csvfile.write("matchingrulesvalues" + delim + val + delim)
-                continue
-        # Unroll matchingrulestag into individual fields with a number next to it
-        if val == "matchingrulestag" and resultList[4] > 1:
-            for i in range(1, resultList[4] + 1):
-                csvfile.write(val + str(i) + delim)
-            continue
-        # Unroll entitieshtagstext into individual fields with a number next to it
-        if val == "entitieshtagstext" and resultList[2] > 1:
-            for i in range(1, resultList[2] + 1):
-                csvfile.write("entitieshtagstext" + str(i) + delim)
-            continue
-        # Unroll entitiesusrmentions into individual fields with a number next to it
-        if val == "entitiesusrmentions":
-            if resultList[3] >= 1:
-                for i in range(1, resultList[3] + 1):
-                    csvfile.write("entitiesusrmentionsidstr" + str(i) + delim + "entitiesusrmentionssname" + str(i) + delim + "entitiesusrmentionsname" + str(i) + delim)
-                continue
-            else:
-                csvfile.write("entitiesusrmentionsidstr" + delim + "entitiesusrmentionssname" + delim + "entitiesusrmentionsname" + delim)
-            continue
-        if val == "entitiesusrmentionsidstr" or val == "entitiesusrmentionssname" or val == "entitiesusrmentionsname":
-            continue
-        # Else write the field name into the file
-        csvfile.write(val + delim)
-    # Write a new line character to denote the end of header line
-    csvfile.write('\n')
-    # Create a Unicode based CSV writer with the opened csvfile and quoting all the values in all the fields
-    writer = CSVUnicodeWriter(csvfile)
-    # Return a writer, keyList tuple
-    return (writer, keyList)
-
-# ========================================================================================
-
-def printCSV(resultList, path, month, rule, rtLines):
-    # Mention the delimiter
-    delim = ","
-    # Retrieve the count from the resultList parameter
-    counter = resultList[5]
-    # Create a new CSV file with the specified name in the specified path and open it as a binary file
-    csvfile = open(path + month + rule + '_' + str(counter) + '.csv', 'wb')
-    conf = ConfigParser.ConfigParser()
-    conf.read(conf_path.format("mongoToFields.cfg"))
-    # Retrieve the CSVUnicodeWriter and the list of all the field names after the header of the CSV file has been printed
-    (writer, keyList) = printHead(csvfile, resultList, delim, conf.items('fields'))
-
-    # Iterate over each record
-    for result in resultList[0]:
-        row = []
-        # For each key in keyList
-        for key in keyList:
-            # If the key exists in the current record
-            if key in result:
-                if key == "_id":
-                    # Edit the tweet id and append the new id to the row list
-                    row.append("tw" + result[key])
-                    continue
-                if key == "pt":
-                    # Edit the postedTime into the postedTime, Year, Month, Day and Time fields
-                    date = result[key]
-                    row.append(str(date))
-                    row.append(date.strftime("%Y"))
-                    row.append(date.strftime("%b"))
-                    row.append(date.strftime("%d"))
-                    row.append(date.strftime("%H:%M:%S"))
-                    continue
-                if key  == "auo":
-                    # If the actorUtcOffset isn't present, add a '.'
-                    if result[key] is None:
-                        row.append('.')
-                        continue
-                # Unroll the matchingrulestag array into multiple fields
-                if key == "mrt":
-                    tags = result[key]
-                    translatedTags = []
-                    for t in tags:
-                        try:
-                            tag = (rtLines[0][int(t)].split(':')[0]).strip('"')
-                            translatedTags.append(tag)
-                        except IndexError:
-                            break
-                    translatedTags += [""] * (resultList[4] - len(translatedTags))
-                    row.extend(translatedTags)
-                    continue
-                # Unroll the matchingrulesvalue array into multiple fields
-                if key == "mrv":
-                    translatedRules = []
-                    rules = []
-                    for j in range(0, resultList[1]):
-                        try:
-                            rule = ':'.join(rtLines[1][int(result[key][j])].split(':')[0:-1])
-                            translatedRules.append(rule)
-                            rules.append(str(result[key][j]))
-                        except IndexError:
-                            break
-                    row.append(';'.join(translatedRules))
-                    rules += [""] * (resultList[1] - len(result[key]))
-                    row.extend(rules)
-                    continue
-                # Unroll the entitiesusrmentions array and its constituent fields into multiple fields
-                if key == "eum":
-                    usrMentions = []
-                    for j in result[key]:
-                        for (k, v) in j.iteritems():
-                            usrMentions.append(v)
-                    usrMentions += ["", "", ""] * (resultList[3] - len(result[key]))
-                    row.extend(usrMentions)
-                    continue
-                # Unroll the entitieshtagstext array into multiple fields
-                if key == "eumh":
-                    htag = result[key]
-                    htag += [""] * (resultList[2] - len(result[key]))
-                    row.extend(htag)
-                    continue
-                row.append(unicode(result[key]))
-            else:
-                # If the key doesn't exist, append an empty string into the row
-                if key == 'eumh':
-                    row.extend([""] * resultList[2])
-                else:
-                    row.append("")
-        # Writer writes the row list
-        writer.writerow(row)
-    # Close the CSV file
-    csvfile.close()
+                
 
 # ========================================================================================
 # Command to run the script
@@ -256,16 +127,6 @@ if __name__ == "__main__":
     # Generate full path for the corresponding year and month
     path = dest.format(year + monthToNames[month], 'CSVRULES')
     mkdir_p(path)
-    # Future work
-    # If time frame based, generate files based on start and end month and rule
-    # if start_time == end_time:
-    #     window = [start_time]
-    #     csvfile = open(start_time + '_' + '.csv', 'w')
-    # else:
-    #     window = [start_time, end_time]
-    #     csvfile = open(start_time + '_' + end_time + '.csv', 'w')
-    # rules = sys.argv[3:]
-    
     # Get the total number of rules and the list of all the rules
     r = open(conf_path.format("tw_rules.json"))
     ruleLines = r.readlines()
