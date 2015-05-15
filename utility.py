1a378bd219aa8765a4b7893379b078ba821bac34
Karthik Hariharan <karthikhariharan13@gmail.com>
2015-05-15 14:55:45 -0500

    Added function to pretty print a dict

--- utility.py
+++ utility.py
@@ -99,3 +99,26 @@ def fbAggregateByDay(year, m1, m2, conf):
                 curr_file.close()
                 curr_file = None
 
+# =============================================================================================
+# Prints python dict object in a nested manner
+def dump(obj, nested_level=0, output=sys.stdout):
+    spacing = '   '
+    if type(obj) == dict:
+        print >> output, '%s{' % ((nested_level) * spacing)
+        for k, v in obj.items():
+            if hasattr(v, '__iter__'):
+                print >> output, '%s%s:' % ((nested_level + 1) * spacing, k)
+                dump(v, nested_level + 1, output)
+            else:
+                print >> output, '%s%s: %s' % ((nested_level + 1) * spacing, k, v)
+        print >> output, '%s}' % (nested_level * spacing)
+    elif type(obj) == list:
+        print >> output, '%s[' % ((nested_level) * spacing)
+        for v in obj:
+            if hasattr(v, '__iter__'):
+                dump(v, nested_level + 1, output)
+            else:
+                print >> output, '%s%s' % ((nested_level + 1) * spacing, v)
+        print >> output, '%s]' % ((nested_level) * spacing)
+    else:
+        print >> output, '%s%s' % (nested_level * spacing, obj)
