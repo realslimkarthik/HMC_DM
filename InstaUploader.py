3e9516cb0444dd8f43d4dc5cab957a9b00a923fd
Karthik Hariharan <karthikhariharan13@gmail.com>
2015-05-26 13:02:22 -0500

    New server specific option

--- InstaUploader.py
+++ InstaUploader.py
@@ -4,5 +4,12 @@ import InstagramClient as insta
 if __name__ == "__main__":
     year = sys.argv[1]
     month = sys.argv[2]
-    uploader = insta.InstagramClient(year, month)
+    try:
+        server = sys.argv[3]
+    except IndexError:
+        server = True
+
+    if server != True:
+        server = False
+    uploader = insta.InstagramClient(year, month, server)
     uploader.iterateOverFiles()
